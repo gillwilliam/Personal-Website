05aa223163037e4b24df86bb3fcd84adcd3df49e
gillwilliam <gill.william@gmail.com>
2017-08-14 21:32:40 -0400

    ultimate responsive experience

--- src/Music.js
+++ src/Music.js
@@ -2,9 +2,19 @@ import Paper from 'material-ui/Paper';
 import TypeWriter from 'react-typewriter';
 import TopElement from './TopElement';
 import HtmlTag from './HtmlTag';
+import Responsive from 'react-responsive';
 
 var React = require('react');
 const Api = require('./Api');
+var MediaQuery = require('react-responsive');
+
+const ExtraLarge = ({ children }) => <Responsive minWidth={1566} children={children} />;
+const ExtraLarge1 = ({ children }) => <Responsive minWidth={1378} maxWidth={1565} children={children} />;
+const ExtraLarge2 = ({ children }) => <Responsive minWidth={1200} maxWidth={1378} children={children} />;
+const Large = ({ children }) => <Responsive minWidth={992} maxWidth={1200} children={children} />;
+const Medium = ({ children }) => <Responsive minWidth={768} maxWidth={992} children={children} />;
+const Small = ({ children }) => <Responsive minWidth={576} maxWidth={768} children={children} />;
+const ExtraSmall = ({ children }) => <Responsive maxWidth={576} children={children} />;
 
 class Music extends React.Component {
 
@@ -20,7 +30,7 @@ class Music extends React.Component {
     Api.getArtists().then((data) => {
       this.setState(() => {
         return {
-          artists: data.slice(0, 8)
+          artists: data
         }
       })
     })
@@ -28,7 +38,7 @@ class Music extends React.Component {
     Api.getAlbums().then((data) => {
       this.setState(() => {
         return {
-          albums: data.slice(0, 8)
+          albums: data
         }
       })
     })
@@ -40,27 +50,43 @@ class Music extends React.Component {
         <div className="center">
           <br/>
 
-            <img src="https://image.flaticon.com/icons/svg/149/149125.svg" style={{
-              height: "150",
-              width: 150
-            }}/>
-
-
+          <img src="https://image.flaticon.com/icons/svg/149/149125.svg" style={{
+            height: "150",
+            width: 150
+          }}/>
 
           <hr/>
+          <ExtraLarge>
+              <GridCreator artists={this.state.artists.slice(0,8)} albums={this.state.albums.slice(0,8)} xl={true}/>
+          </ExtraLarge>
 
-          <div className="popular-list">
-            {this.state.artists.map((artist) => {
-              return (<MusicGrid img={artist.image[3]["#text"]} artistName={artist.name.substring(0,19)} playCount={artist.playcount} url={artist.url} artist={true}/>)
-            })}
-          </div>
+          <ExtraLarge1>
+              <GridCreator artists={this.state.artists.slice(0,7)} albums={this.state.albums.slice(0,7)} xl={true}/>
+          </ExtraLarge1>
+
+          <ExtraLarge2>
+              <GridCreator artists={this.state.artists.slice(0,6)} albums={this.state.albums.slice(0,6)} xl={true}/>
+          </ExtraLarge2>
+
+          <Large>
+              <GridCreator artists={this.state.artists.slice(0,9)} albums={this.state.albums.slice(0,9)}/>
+          </Large>
+
+          <Medium>
+              <GridCreator artists={this.state.artists.slice(0,9)} albums={this.state.albums.slice(0,9)}/>
+          </Medium>
+
+          <Small>
+              <GridCreator artists={this.state.artists.slice(0,6)} albums={this.state.albums.slice(0,6)}/>
+          </Small>
+
+          <ExtraSmall>
+              <GridCreator artists={this.state.artists.slice(0,6)} albums={this.state.albums.slice(0,6)}/>
+          </ExtraSmall>
+
 
           <br/>
-          <div className="popular-list">
-            {this.state.albums.map((albums) => {
-              return (<MusicGrid img={albums.image[3]["#text"]} artistName={albums.name.substring(0,19)} playCount={albums.playcount} url={albums.url} artist={false}/>)
-            })}
-          </div>
+
 
         </div>
       </div>
@@ -68,11 +94,29 @@ class Music extends React.Component {
   }
 }
 
+function GridCreator(props) {
+  return (
+    <div>
+    <div className={props.xl ? "popular-list" : "row"}>
+      {props.artists.map((artist) => {
+        return (<MusicGrid key={artist.name} img={artist.image[3]["#text"]} artistName={artist.name.substring(0, 19)} playCount={artist.playcount} url={artist.url} artist={true} xl={props.xl}/>)
+      })}
+    </div>
+
+      <div className={props.xl ? "popular-list" : "row"}>
+        {props.albums.map((albums) => {
+          return (<MusicGrid img={albums.image[3]["#text"]} artistName={albums.name.substring(0, 19)} playCount={albums.playcount} url={albums.url} artist={false} xl={props.xl}/>)
+        })}
+      </div>
+    </div>
+  )
+}
+
 class MusicGrid extends React.Component {
 
   render() {
     return (
-      <ul className='space-list-items'>
+      <ul className={this.props.xl ? "space-list-items" : 'col-lg-2 col-md-4 col-sm-4 col-xs-6'}>
         <li>
           <img className={this.props.artist
             ? 'Artist-Pic'
@@ -83,7 +127,8 @@ class MusicGrid extends React.Component {
         </li>
         <li style={{
           fontWeight: "bold"
-        }}>{this.props.playCount} plays</li>
+        }}>{this.props.playCount}
+          plays</li>
       </ul>
     )
   }
